f5226b44757ebe809a2c54665cc6849d7147c8da
Peter Farley <far.peter1@gmail.com>
2016-10-11 16:46:46 -0500

    Made basic prompt program using two 'functions'

--- hsq_files/funcs_test/funcs.sh
+++ hsq_files/funcs_test/funcs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat main.hsq funcs.hsq > _funcs.hsq
+cat main.hsq func_*.hsq globals.hsq > _funcs.hsq
 ../../subleq_asm_compiler -i _funcs.hsq -o _funcs.sq -v
 ../../../subleq_assembler/subleq_assembler -i _funcs.sq -o funcs.bin -b 2 -E 0x200 -d _funcs.dbg
